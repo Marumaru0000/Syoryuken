54aa5d372f181cf7d2c12d433ddf96fb1d73fd3d
Marumaru0000 <c-kmaruyama@yahoo-corp.jp>
2023-08-27 17:59:51 +0900

    初課題！平日に作業できないので頑張りました！

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var attacks: [UIImageView]!
+    
+    var currentAttackIndex = 0
+    let maxAttackIndex = 19 // 0から19までの20枚の画像
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+                for attack in attacks {
+                    attack.isUserInteractionEnabled = true
+                    attack.addGestureRecognizer(tapGesture)
+                }
+            }
+            
+    @objc func handleTap() {
+        currentAttackIndex += 1
+        if currentAttackIndex > maxAttackIndex {
+            currentAttackIndex = 0
+        }
+        
+        for attack in attacks {
+            let imageName = "attack\(currentAttackIndex + 1)" // attack1からattack20まで
+            attack.image = UIImage(named: imageName)
+        }
+        
     }
-
-
+    
+    @IBAction func attackButton(_ sender: UIButton) {
+        handleTap()
+    }
+    
 }
 
