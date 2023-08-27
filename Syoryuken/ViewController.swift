74539ddd6654c6d7839259af10ec8aaac6ffd1bc
Marumaru0000 <c-kmaruyama@yahoo-corp.jp>
2023-08-27 23:02:05 +0900

    平日の分まで頑張りました。おやすみなさい！

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -1,46 +1,30 @@
-//
-//  ViewController.swift
-//  Syoryuken
-//
-//  Created by 丸山航輝 on 2023/08/27.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet var attacks: [UIImageView]!
-    
     var currentAttackIndex = 0
-    let maxAttackIndex = 19 // 0から19までの20枚の画像
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-                for attack in attacks {
-                    attack.isUserInteractionEnabled = true
-                    attack.addGestureRecognizer(tapGesture)
-                }
-            }
-            
-    @objc func handleTap() {
-        currentAttackIndex += 1
-        if currentAttackIndex > maxAttackIndex {
-            currentAttackIndex = 0
-        }
-        
-        for attack in attacks {
-            let imageName = "attack\(currentAttackIndex + 1)" // attack1からattack20まで
-            attack.image = UIImage(named: imageName)
-        }
-        
+        // 初期の画像を設定
+        let initialImage = UIImage(named: "attack1")
+        attacks[0].image = initialImage
     }
-    
-    @IBAction func attackButton(_ sender: UIButton) {
-        handleTap()
-    }
-    
-}
 
+    @IBAction func attackButton(_ sender: UIButton) {
+        // 画像の番号を増やす
+        currentAttackIndex += 1
+        
+        // attack20まで行ったらattack1に戻る
+        if currentAttackIndex > 20 {
+            currentAttackIndex = 1
+        }
+        
+        // 新しい画像を設定
+        let imageName = "attack\(currentAttackIndex)"
+        let newImage = UIImage(named: imageName)
+        attacks[0].image = newImage
+    }
+}
